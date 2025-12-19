88905a4634890360f7c80fc9334cbc74d3139548
almukhanbetov <almuko.m@gmail.com>
2025-12-19 12:06:14 +0500

    migrate assets to Vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,11 +9,8 @@
     <meta name="description" content="" />
     <meta name="keywords" content="bootstrap, bootstrap4" />
     <!-- Bootstrap CSS -->
-    <link href="{{ asset('ddl/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <link href="{{ asset('ddl/css/tiny-slider.css') }}" rel="stylesheet">
-    <link href="{{ asset('ddl/css/style.css') }}" rel="stylesheet">
     <title>Главная</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
@@ -21,10 +18,7 @@
     @include('includes.hero')
     @yield('content')
     @include('includes.footer')
-    <script src="{{ asset('ddl/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('ddl/js/tiny-slider.js') }}"></script>
-    <script src="{{ asset('ddl/js/custom.js') }}"></script>
-    <script src="{{ asset('ddl/js/video.js') }}"></script>
+
 </body>
 
 </html>

b3cd3f926b5fe4f54865e36afddf3acde8ee4d28
almukhanbetov <almuko.m@gmail.com>
2025-12-26 21:06:24 +0500

    'build' => env('APP_BUILD'),

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,16 @@ use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\BookingController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\VisitController;
-
+Route::get('/version', function () {
+    return [
+        'build' => env('APP_BUILD'),
+    ];
+});
 Route::get("/", [PageController::class, 'index'])->name('pages.index');
 Route::get("/pages/shop", [PageController::class, 'shop'])->name('pages.shop');
 Route::get("/pages/about", [PageController::class, 'about'])->name('pages.about');
 Route::get("/pages/contacts", [PageController::class, 'contacts'])->name('pages.contacts');
-Route::get('/version', function () {
-    return [
-        'app' => config('app.name'),
-        'env' => config('app.env'),
-        'build' => env('APP_BUILD'),
-        'time' => now()->toDateTimeString(),
-    ];
-});
+
 
 Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
         Route::get('/', [AdminController::class, 'index'])->name('dashboard');
